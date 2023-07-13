a63c5eca21caf3670f6c5e16299977f5aaea1fe8
Lennie Nurse <lnursej@gmail.com>
2023-07-12 20:17:45 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css"
+import { useEffect } from "react"
 
 function App() {
-  return <div className="App">Hello World!!!</div>
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://localhost:9000")
+      const data = await res.json()
+      console.log(data)
+    }
+    fetchData()
+  }, [])
+  return (
+    <div className="App">
+      <h1>Hello World!!!3</h1>
+    </div>
+  )
 }
 
 export default App
