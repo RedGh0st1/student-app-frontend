124724dcb794c2714955191136e0f43a47de1d55
Lennie Nurse <lnursej@gmail.com>
2023-07-16 16:37:17 -0400

    updated student list to add css

--- src/components/StudentList/StudentList.js
+++ src/components/StudentList/StudentList.js
@@ -1,11 +1,12 @@
 import StudentCard from "../StudentCard/StudentCard"
+import "../StudentList/StudentList.css"
 
 export default function StudentList({ studentData }) {
   console.log("<studentlist /> rendered!")
   return (
-    <div className="studentlists">
+    <div className="StudentList">
       {studentData.map((student) => (
-        <StudentCard student={student} />
+        <StudentCard key={student.id} student={student} />
       ))}
     </div>
   )
