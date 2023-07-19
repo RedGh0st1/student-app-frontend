3ce84b0b83dd24d9b37ef78451fb4c5ecccf8aea
Lennie Nurse <lnursej@gmail.com>
2023-07-19 19:27:24 -0400

    added loading state

--- src/App.js
+++ src/App.js
@@ -8,25 +8,35 @@ const API_URL = "http://localhost:9000"
 
 function App() {
   const [studentData, setStudentData] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     console.log("<App/> useEffect() fired!")
     async function fetchData() {
       // You can await here
+      setLoading(true)
+      // Show the user that we're loading
       const response = await fetch(`${API_URL}/students`)
       const json = await response.json()
       console.log("<App /> useEffect() fetched data", json)
       const { data } = json
       setStudentData(data)
+      // Stop showing the user the loading UI...
+      setLoading(false)
     }
     fetchData()
   }, [])
 
-  console.log(`<App /> rendered! num students = ${studentData.length}`)
+  console.log(
+    `<App /> rendered! loading = ${loading},  num students = ${studentData.length}`
+  )
   return (
     <div className="App">
+      {/* If loading, render <loading /> else render <StudentList />*/}
+      {loading ? <Loading />
       <h1>Student List</h1>
       <StudentList studentData={studentData} />
+      : null}
     </div>
   )
 }
